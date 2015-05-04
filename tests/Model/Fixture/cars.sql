4783e0af1baa6a882ddfa3ebc4aa00806546ac8f
René Tønner Stokholm <zortje@gmail.com>
2015-05-04 19:27:15 +0200

    Adding cars sql file for table creation

---
+++ tests/Model/Fixture/cars.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `cars` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`make` VARCHAR(64) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`model` VARCHAR(64) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`hp` INT(10) UNSIGNED NOT NULL,
+	`modified` DATETIME NOT NULL,
+	`created` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;

462ece8692265d03dc31c781035ec021b7cab245
René Tønner Stokholm <zortje@gmail.com>
2015-05-17 03:10:15 +0200

    Adding base  controller stub

---
+++ src/Controller/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zortje\MVC\Controller;
+
+/**
+ * Class Controller
+ *
+ * @package Zortje\MVC\Controller
+ */
+class Controller {
+
+}
---
+++ tests/Controller/Fixture/CarsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zortje\MVC\Tests\Controller\Fixture;
+
+use Zortje\MVC\Controller\Controller;
+
+/**
+ * Class CarsController
+ *
+ * @package Zortje\MVC\Tests\Controller\Fixture
+ */
+class CarsController extends Controller {
+
+}

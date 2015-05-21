d83579cbf791d3ee72b787b987583a77d0078a8e
René Tønner Stokholm <zortje@gmail.com>
2015-05-21 22:31:40 +0200

    Finishing up basic dispatcher logic

--- src/Routing/Dispatcher.php
+++ src/Routing/Dispatcher.php
@@ -3,13 +3,16 @@
 namespace Zortje\MVC\Routing;
 
 use Zortje\MVC\Controller\ControllerFactory;
+use Zortje\MVC\Controller\NotFoundController;
+use Zortje\MVC\Model\User;
+use Zortje\MVC\Network\Request;
+use Zortje\MVC\Network\Response;
 use Zortje\MVC\Controller\Exception\ControllerNonexistentException;
 use Zortje\MVC\Controller\Exception\ControllerInvalidSuperclassException;
 use Zortje\MVC\Controller\Exception\ControllerActionProtectedInsufficientAuthenticationException;
 use Zortje\MVC\Controller\Exception\ControllerActionPrivateInsufficientAuthenticationException;
 use Zortje\MVC\Controller\Exception\ControllerActionNonexistentException;
-use Zortje\MVC\Network\Request;
-use Zortje\MVC\Network\Response;
+use Zortje\MVC\Routing\Exception\RouteNonexistentException;
 
 /**
  * Class Dispatcher
@@ -23,63 +26,73 @@ class Dispatcher {
 	 */
 	protected $router;
 
+	/**
+	 * @var \PDO PDO
+	 */
+	protected $pdo;
+
+	/**
+	 * @var null|User User
+	 */
+	protected $user;
+
+	/**
+	 * @param Request $request Request object
+	 *
+	 * @return Response Reponse object
+	 *
+	 * @throws \Exception If unexpected exception is thrown
+	 */
 	public function dispatch(Request $request) {
-
-		// @todo
-
-		list($controller, $action) = $this->router->route($request->getUrlPath());
-
-		/**
-		 * Validate and initialize controller
-		 */
-		$controllerFactory = new ControllerFactory($pdo, $user);
+		$controllerFactory = new ControllerFactory($this->pdo, $this->user);
 
 		try {
-			$controllerFactory = new ControllerFactory();
+			list($controller, $action) = array_values($this->router->route($request->getPath()));
 
-			$controller = $controllerFactory->create($controller);
-		} catch (\Exception $e) {
-			if ($e instanceof ControllerNonexistentException || $e instanceof ControllerInvalidSuperclassException) {
-				// @todo Log nonexistent controller
-				// @todo Log invalid superclass
+			/**
+			 * Validate and initialize controller
+			 */
+			try {
+				$controller = $controllerFactory->create($controller);
+			} catch (\Exception $e) {
+				if ($e instanceof ControllerNonexistentException || $e instanceof ControllerInvalidSuperclassException) {
+					// @todo Log nonexistent controller
+					// @todo Log invalid superclass
 
-				$controller = $controllerFactory->create('NotFound');
-			} else {
-				throw $e;
+					$controller = $controllerFactory->create(NotFoundController::class);
+					$action     = 'index';
+				} else {
+					throw $e;
+				}
 			}
+		} catch (RouteNonexistentException $e) {
+			$controller = $controllerFactory->create(NotFoundController::class);
+			$action     = 'index';
 		}
 
-
+		/**
+		 * Validate and set controller action
+		 */
 		try {
-
-			// Check if controller implements the action
-			// Check if user is properly authenticated for that action
-
-			$controller->prepareAction($action);
-
+			$controller->setAction($action);
 		} catch (ControllerActionProtectedInsufficientAuthenticationException $e) {
-			// @todo Log unauthed protected controller action attempt
+			// @todo redirect to login page & and save what action was requested to redirect after successful login
 
-			// redirect to login page & and save what action was requested to redirect after successful login
+			// @todo Log unauthed protected controller action attempt
 		} catch (ControllerActionPrivateInsufficientAuthenticationException $e) {
 			// @todo Log unauthed private controller action attempt
 
-			$controller = $controllerFactory->create('NotFound');
+			$controller = $controllerFactory->create(NotFoundController::class);
 		} catch (ControllerActionNonexistentException $e) {
 			// @todo Log nonexistent controller action
 
-			$controller = $controllerFactory->create('NotFound');
+			$controller = $controllerFactory->create(NotFoundController::class);
 		}
 
-
-		// The content type is decided by the controller and is sent along in the $headers array
-		// Request type could be:
-		//
-		// text/html
-		// application/javascript
-
+		/**
+		 * Create response from controller action headers and output
+		 */
 		list($headers, $output) = $controller->callAction();
-
 
 		$response = new Response($headers, $output);
 
@@ -88,9 +101,13 @@ class Dispatcher {
 
 	/**
 	 * @param Router $router
+	 * @param \PDO   $pdo
+	 * @param User   $user
 	 */
-	public function __construct(Router $router) {
+	public function __construct(Router $router, \PDO $pdo, User $user = null) {
 		$this->router = $router;
+		$this->pdo    = $pdo;
+		$this->user   = $user;
 	}
 
 }

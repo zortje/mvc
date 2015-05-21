5ff3cbda229a50f5eef38d1c6741576d04bf4598
René Tønner Stokholm <zortje@gmail.com>
2015-05-21 22:32:11 +0200

    Adding properties and function stubs

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace Zortje\MVC\Controller;
 
+use Zortje\MVC\Model\User;
+
 /**
  * Class Controller
  *
@@ -25,5 +27,45 @@ class Controller {
 	 * Will result in an 404 if not authenticated
 	 */
 	const ACTION_PRIVATE = 2;
+
+	/**
+	 * @var \PDO PDO
+	 */
+	protected $pdo;
+
+	/**
+	 * @var null|User User
+	 */
+	protected $user;
+
+	/**
+	 * @param string $action Controller action
+	 */
+	public function setAction($action) {
+
+		// Check if controller implements the action
+		// Check if user is properly authenticated for that action
+
+
+	}
+
+	public function callAction() {
+		// The content type is decided by the controller and is sent along in the $headers array
+		// Request type could be:
+		//
+		// text/html
+		// application/javascript
+
+		// @todo throw exception is action is not set
+	}
+
+	/**
+	 * @param \PDO      $pdo
+	 * @param null|User $user
+	 */
+	public function __construct(\PDO $pdo, User $user = null) {
+		$this->pdo  = $pdo;
+		$this->user = $user;
+	}
 
 }

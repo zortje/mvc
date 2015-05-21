b8acdf8edbcfc86568fe83dce9731d98bc40aaf4
René Tønner Stokholm <zortje@gmail.com>
2015-05-21 22:35:56 +0200

    Adding not found controller and user model

---
+++ src/Controller/NotFoundController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zortje\MVC\Controller;
+
+/**
+ * Class NotFoundController
+ *
+ * @package Zortje\MVC\Controller
+ */
+class NotFoundController extends Controller {
+
+	protected $access = [
+		'index' => Controller::ACTION_PUBLIC
+	];
+
+}
---
+++ src/Model/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zortje\MVC\Model;
+
+use Zortje\MVC\Model\Table\Entity\Entity;
+
+/**
+ * Class User
+ *
+ * @package Zortje\MVC\Model
+ */
+class User extends Entity {
+
+}

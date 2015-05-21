821064610408c96066fd03237e1bf646af68b40d
René Tønner Stokholm <zortje@gmail.com>
2015-05-21 21:35:36 +0200

    Adding tests

--- src/Controller/ControllerFactory.php
+++ src/Controller/ControllerFactory.php
@@ -4,6 +4,7 @@ namespace Zortje\MVC\Controller;
 
 use Zortje\MVC\Controller\Exception\ControllerNonexistentException;
 use Zortje\MVC\Controller\Exception\ControllerInvalidSuperclassException;
+use Zortje\MVC\Model\User;
 
 /**
  * Class ControllerFactory
@@ -11,6 +12,16 @@ use Zortje\MVC\Controller\Exception\ControllerInvalidSuperclassException;
  * @package Zortje\MVC\Controller
  */
 class ControllerFactory {
+
+	/**
+	 * @var \PDO PDO
+	 */
+	protected $pdo;
+
+	/**
+	 * @var null|User User
+	 */
+	protected $user;
 
 	/**
 	 * Initialize controller
@@ -24,14 +35,26 @@ class ControllerFactory {
 	 */
 	public function create($controller) {
 		if (!class_exists($controller)) {
-			throw new ControllerNonexistentException($controller);
+			throw new ControllerNonexistentException([$controller]);
 		} else if (!is_subclass_of($controller, Controller::class)) {
-			throw new ControllerInvalidSuperclassException($controller);
+			throw new ControllerInvalidSuperclassException([$controller]);
 		}
 
-		$controller = new $controller;
+		/**
+		 * @var Controller $controller
+		 */
+		$controller = new $controller($this->pdo, $this->user);
 
 		return $controller;
+	}
+
+	/**
+	 * @param \PDO      $pdo
+	 * @param null|User $user
+	 */
+	public function __construct(\PDO $pdo, User $user = null) {
+		$this->pdo  = $pdo;
+		$this->user = $user;
 	}
 
 }
---
+++ tests/Controller/ControllerFactoryTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Zortje\MVC\Tests\Controller;
+
+use Zortje\MVC\Controller\ControllerFactory;
+use Zortje\MVC\Model\User;
+use Zortje\MVC\Tests\Controller\Fixture\CarsController;
+use Zortje\MVC\Tests\Model\Fixture\CarEntity;
+
+/**
+ * Class ControllerFactoryTest
+ *
+ * @package            Zortje\MVC\Tests\Controller
+ *
+ * @coversDefaultClass Zortje\MVC\Controller\ControllerFactory
+ */
+class ControllerFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	public function setUp() {
+		$this->pdo = new \PDO("mysql:host=127.0.0.1;dbname=myapp_test", 'root', '');
+	}
+
+	/**
+	 * @covers ::create
+	 */
+	public function testCreate() {
+		$controllerFactory = new ControllerFactory($this->pdo, null);
+
+		$controller = $controllerFactory->create(CarsController::class);
+
+		$this->assertSame(get_class($controller), CarsController::class);
+	}
+
+	/**
+	 * @covers ::create
+	 *
+	 * @expectedException Zortje\MVC\Controller\Exception\ControllerNonexistentException
+	 * @expectedExceptionMessage Controller NonexistentController is nonexistent
+	 */
+	public function testCreateNonexistent() {
+		$controllerFactory = new ControllerFactory($this->pdo, null);
+
+		$controllerFactory->create('NonexistentController');
+	}
+
+	/**
+	 * @covers ::create
+	 *
+	 * @expectedException Zortje\MVC\Controller\Exception\ControllerInvalidSuperclassException
+	 * @expectedExceptionMessage Controller Zortje\MVC\Tests\Model\Fixture\CarEntity is not a subclass of Controller
+	 */
+	public function testCreateInvalidSuperclass() {
+		$controllerFactory = new ControllerFactory($this->pdo, null);
+
+		$controllerFactory->create(CarEntity::class);
+	}
+
+	/**
+	 * @covers ::__construct
+	 */
+	public function testConstruct() {
+		$user = new User(null, new \DateTime(), new \DateTime());
+
+		$controllerFactory = new ControllerFactory($this->pdo, $user);
+
+		$reflector = new \ReflectionClass($controllerFactory);
+
+		$pdoProperty = $reflector->getProperty('pdo');
+		$pdoProperty->setAccessible(true);
+		$this->assertSame($this->pdo, $pdoProperty->getValue($controllerFactory));
+
+		$userProperty = $reflector->getProperty('user');
+		$userProperty->setAccessible(true);
+		$this->assertSame($user, $userProperty->getValue($controllerFactory));
+	}
+
+}

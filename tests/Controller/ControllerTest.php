da95d84a19bba7215eb6c8f6dc9b2a2a24038d24
René Tønner Stokholm <zortje@gmail.com>
2016-02-28 22:09:03 +0100

    Adding tests

--- tests/Controller/ControllerTest.php
+++ tests/Controller/ControllerTest.php
@@ -3,7 +3,11 @@
 namespace Zortje\MVC\Tests\Controller;
 
 use Zortje\MVC\Controller\ControllerFactory;
+use Zortje\MVC\Controller\Exception\ControllerActionNonexistentException;
+use Zortje\MVC\Controller\Exception\ControllerActionPrivateInsufficientAuthenticationException;
+use Zortje\MVC\Controller\Exception\ControllerActionProtectedInsufficientAuthenticationException;
 use Zortje\MVC\Tests\Controller\Fixture\CarsController;
+use Zortje\MVC\User\User;
 
 /**
  * Class ControllerTest
@@ -26,6 +30,44 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @covers ::__construct
+     */
+    public function testConstruct()
+    {
+        $user = new User('', '');
+
+        $controllerFactory = new ControllerFactory($this->pdo, '/var/www/html/', $user);
+
+        $controller = $controllerFactory->create(CarsController::class);
+
+        $reflector = new \ReflectionClass($controller);
+
+        $pdoProperty = $reflector->getProperty('pdo');
+        $pdoProperty->setAccessible(true);
+        $this->assertSame($this->pdo, $pdoProperty->getValue($controller));
+
+        $appPathProperty = $reflector->getProperty('appPath');
+        $appPathProperty->setAccessible(true);
+        $this->assertSame('/var/www/html/', $appPathProperty->getValue($controller));
+
+        $userProperty = $reflector->getProperty('user');
+        $userProperty->setAccessible(true);
+        $this->assertSame($user, $userProperty->getValue($controller));
+    }
+
+    /**
+     * @covers ::getShortName
+     */
+    public function testGetShortName()
+    {
+        $controllerFactory = new ControllerFactory($this->pdo, '', null);
+
+        $controller = $controllerFactory->create(CarsController::class);
+
+        $this->assertSame('Cars', $controller->getShortName());
+    }
+
+    /**
      * @covers ::setAction
      */
     public function testSetAction()
@@ -37,19 +79,19 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
 
         $reflector = new \ReflectionClass($carsController);
 
-        $action = $reflector->getProperty('action');
-        $action->setAccessible(true);
-        $this->assertSame('index', $action->getValue($carsController));
+        $property = $reflector->getProperty('action');
+        $property->setAccessible(true);
+        $this->assertSame('index', $property->getValue($carsController));
     }
 
     /**
      * @covers ::setAction
-     *
-     * @expectedException Zortje\MVC\Controller\Exception\ControllerActionNonexistentException
-     * @expectedExceptionMessage Controller Zortje\MVC\Tests\Controller\Fixture\CarsController action nonexistent is nonexistent
      */
     public function testSetActionNonexistent()
     {
+        $this->expectException(ControllerActionNonexistentException::class);
+        $this->expectExceptionMessage('Controller Zortje\MVC\Tests\Controller\Fixture\CarsController action nonexistent is nonexistent');
+
         $controllerFactory = new ControllerFactory($this->pdo, '', null);
 
         $carsController = $controllerFactory->create(CarsController::class);
@@ -58,12 +100,12 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers ::setAction
-     *
-     * @expectedException Zortje\MVC\Controller\Exception\ControllerActionProtectedInsufficientAuthenticationException
-     * @expectedExceptionMessage Controller Zortje\MVC\Tests\Controller\Fixture\CarsController protected action hidden requires authentication
      */
     public function testSetActionUnauthenticatedProtected()
     {
+        $this->expectException(ControllerActionProtectedInsufficientAuthenticationException::class);
+        $this->expectExceptionMessage('Controller Zortje\MVC\Tests\Controller\Fixture\CarsController protected action hidden requires authentication');
+
         $controllerFactory = new ControllerFactory($this->pdo, '', null);
 
         $carsController = $controllerFactory->create(CarsController::class);
@@ -72,16 +114,41 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers ::setAction
-     *
-     * @expectedException Zortje\MVC\Controller\Exception\ControllerActionPrivateInsufficientAuthenticationException
-     * @expectedExceptionMessage Controller Zortje\MVC\Tests\Controller\Fixture\CarsController private action add requires authentication
      */
     public function testSetActionUnauthenticatedPrivate()
     {
+        $this->expectException(ControllerActionPrivateInsufficientAuthenticationException::class);
+        $this->expectExceptionMessage('Controller Zortje\MVC\Tests\Controller\Fixture\CarsController private action add requires authentication');
+
         $controllerFactory = new ControllerFactory($this->pdo, '', null);
 
         $carsController = $controllerFactory->create(CarsController::class);
         $carsController->setAction('add');
+    }
+
+    // @todo test ::callAction
+
+    // @todo test ::beforeAction
+
+    // @todo test ::afterAction
+
+    // @todo test ::set
+
+    /**
+     * @covers ::getLayoutTemplate
+     */
+    public function testGetLayoutTemplate()
+    {
+        $controllerFactory = new ControllerFactory($this->pdo, '/var/www/html/', null);
+
+        $carsController = $controllerFactory->create(CarsController::class);
+
+        $reflector = new \ReflectionClass($carsController);
+
+        $method = $reflector->getMethod('getLayoutTemplate');
+        $method->setAccessible(true);
+
+        $this->assertSame('/var/www/html/../tests/View/Layout/Fixture/auto.layout', $method->invoke($carsController));
     }
 
     /**
@@ -122,5 +189,84 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
         $method->setAccessible(true);
 
         $this->assertSame('/src/View/Cars/index.view', $method->invoke($carsController));
+    }
+
+    /**
+     * @param int    $code
+     * @param string $responseCode
+     *
+     * @covers ::setResponseCode
+     *
+     * @dataProvider setResponseCodeProvider
+     */
+    public function testSetResponseCode(int $code, string $responseCode)
+    {
+        $controllerFactory = new ControllerFactory($this->pdo, '', null);
+
+        $carsController = $controllerFactory->create(CarsController::class);
+
+        $reflector = new \ReflectionClass($carsController);
+
+        /**
+         * Set headers property to an empty array
+         */
+        $property = $reflector->getProperty('headers');
+        $property->setAccessible(true);
+
+        $property->setValue($carsController, []);
+
+        /**
+         * Set response
+         */
+        $method = $reflector->getMethod('setResponseCode');
+        $method->setAccessible(true);
+
+        $method->invoke($carsController, $code);
+
+        /**
+         * Assert headers value
+         */
+        $expected = [
+            'response_code' => $responseCode
+        ];
+
+        $this->assertSame($expected, $property->getValue($carsController));
+    }
+
+    /**
+     * Provides test data for testSetResponseCode
+     * @return array
+     */
+    public function setResponseCodeProvider(): array
+    {
+        return [
+            [200, 'HTTP/1.1 200 OK'],
+            [403, 'HTTP/1.1 403 Forbidden'],
+            [404, 'HTTP/1.1 404 Not Found'],
+            [500, 'HTTP/1.1 500 Internal Server Error']
+        ];
+    }
+
+    /**
+     * @covers ::setResponseCode
+     */
+    public function testSetResponseCodeInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('HTTP status \'42\' is not implemented');
+
+        $controllerFactory = new ControllerFactory($this->pdo, '', null);
+
+        $carsController = $controllerFactory->create(CarsController::class);
+
+        $reflector = new \ReflectionClass($carsController);
+
+        /**
+         * Set response
+         */
+        $method = $reflector->getMethod('setResponseCode');
+        $method->setAccessible(true);
+
+        $method->invoke($carsController, 42);
     }
 }
